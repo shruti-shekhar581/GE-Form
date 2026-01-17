da8cc190691ebe151a3d4b1c0eb5d591a75f74e6
shruti-shekhar581 <shrutishekhar65@gmail.com>
2026-01-17 23:07:39 +0530

    Test commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-red-600 text-white py-6">
+      <div></div>
       <div className="container mx-auto px-4">
         <p className="text-center">© 2023 Your Company Name. All rights reserved.</p>
       </div>
